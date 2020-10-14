0dd52dc665b83724afc9d5a2c5c9cfdec6dd076c
ayako.okuno <a98@electro89.com>
2020-10-14 11:03:44 +0900

    Initial Commit

---
+++ SlideShowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideShowApp
+//
+//  Created by ELECTRO89 on 2020/10/14.
+//  Copyright © 2020 ELECTRO89. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
