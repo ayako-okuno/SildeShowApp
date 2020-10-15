71d29fbb41d6154665571cb4df4f58d36ee53ab3
ayako.okuno <a98@electro89.com>
2020-10-15 12:33:47 +0900

    2回目：ResultViewControllerのコードを変更しました。

--- SlideShowApp/ResultViewController.swift
+++ SlideShowApp/ResultViewController.swift
@@ -15,44 +15,13 @@ class ResultViewController: UIViewController {
     
     var nowIndex:Int = 0
     var scale:CGFloat = 1.0
-    var width:CGFloat = 0
-    var height:CGFloat = 0
-    var screenWidth:CGFloat = 0
-    var screenHeight:CGFloat = 0
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-            screenWidth = self.view.bounds.width
-            screenHeight = self.view.bounds.height
-            
-            let image = UIImage(named:"dog1")!
-            
-            width = image.size.width
-            height = image.size.height
-            
+    let image = UIImage(named:"dog1big")!
             imageView2 = UIImageView(image:image)
             
-            scale = screenWidth / width
-            let rect:CGRect = CGRect(x:20, y:30, width:374, height:561)
-            
-            imageView2.frame = rect;
-            imageView2.center = CGPoint(x:screenWidth/2, y:screenHeight/2)
-            
-            self.view.addSubview(imageView2)
-    
-            if(374 < screenWidth*2 ){
-                scale += 0.2
-            }
-        
-            if(561 < screenHeight*2 ){
-                           scale += 0.2
-            }
-        
-            imageView2.frame = rect;
-            imageView2.center = CGPoint(x:screenWidth/2, y:screenHeight/2)
-            self.view.addSubview(imageView2)
-        
             }
     
         @IBAction func backButton(_ sender: Any) {
