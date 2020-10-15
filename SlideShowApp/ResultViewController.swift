ed9cdbad937e59d967351af40ee223d8a35ea487
ayako.okuno <a98@electro89.com>
2020-10-15 16:48:39 +0900

    原因不明：resultviewcontrollerの画像が実装すると拡大したはずなのに小さく表示されます。

--- SlideShowApp/ResultViewController.swift
+++ SlideShowApp/ResultViewController.swift
@@ -10,26 +10,16 @@ import UIKit
 
 class ResultViewController: UIViewController {
     
-    var imageView2:UIImageView!
-    var backButton:UIButton!
-    
-    var nowIndex:Int = 0
-    var scale:CGFloat = 1.0
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var backButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    let image = UIImage(named:"dog1big")!
-            imageView2 = UIImageView(image:image)
-            
-            }
-    
-        @IBAction func backButton(_ sender: Any) {
-            backButton.setTitle("戻る", for: .normal)
-            nowIndex = 0
-            if nowIndex == 0 {
-            self.performSegue(withIdentifier: "toFirst", sender: self)
-                
-        }
-    }
-}
+          
+          }
+          @IBAction func Button(_ sender: UIButton) {
+          self.performSegue(withIdentifier: "toFirst", sender: nil)
+          }
+          
+      }
+
